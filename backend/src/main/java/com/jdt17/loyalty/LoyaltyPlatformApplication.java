4e2b9bd4fef4dfa4c5da2cce695b3e68797dbb32
jtnqr <juliuspanjul25@gmail.com>
2026-07-15 15:29:10 +0700

    feat: enable caching on application startup

--- backend/src/main/java/com/jdt17/loyalty/LoyaltyPlatformApplication.java
+++ backend/src/main/java/com/jdt17/loyalty/LoyaltyPlatformApplication.java
@@ -2,10 +2,12 @@ package com.jdt17.loyalty;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableScheduling
+@EnableCaching
 public class LoyaltyPlatformApplication {
 
 	public static void main(String[] args) {
